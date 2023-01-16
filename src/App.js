27e98f1a308214ea9ce38d096e36c946c3de0a63
Martin <martinmarescr@gmail.com>
2023-01-16 03:59:42 -0600

    Form created responsive

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className='contact-box'>
+        <div className='left'></div>
+        <div className='right'>
+          <h2>Contact me</h2>
+          <input className='field' type='text' placeholder='Your name ...'/>
+          <input className='field' type='text' placeholder='Your email ...'/>
+          <input className='field' type='text' placeholder='Your phone ...'/>
+          <textarea className='field area' placeholder='Type your message here...'/>
+          <button className='btn'>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
